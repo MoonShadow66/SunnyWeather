9cf895ca2e8c272b4577f7fc7f208faf97f17b95
pengliang <3495268188@qq.com>
2022-08-04 15:33:40 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/liang/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/liang/sunnyweather/ui/place/PlaceFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.liang.sunnyweather.MainActivity
 import com.liang.sunnyweather.R
 import com.liang.sunnyweather.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -35,7 +36,7 @@ class PlaceFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // 判断是否存已经存储了城市信息
-        if (viewModel.isPlaceSaved()) {
+        if (activity is MainActivity && viewModel.isPlaceSaved()) {
             val place = viewModel.getSavedPlace()
             val intent = Intent(context, WeatherActivity::class.java).apply {
                 putExtra("location_lng", place.location.lng)
